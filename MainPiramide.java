9ddc17d6f4c6af8ef90168e0444d342550c8a148
Maria-Clara-Bertoli <mariaclarabertoli@outlook.com>
2022-10-21 09:45:04 -0300

    Desenvolvimentos com figuras
    Segundo commit

--- MainPiramide.java
+++ MainPiramide.java
@@ -20,6 +20,22 @@ public class MainPiramide {
 		System.out.println("Tipo 3 - R$344,34");
 		int tipo = valor.nextInt();
 		
+		Piramide piramide = new Piramide(altura, raio, tipo);
+		
+		System.out.println("---------------PIRÂMIDE---------------");
+		System.out.println("Altura: " + piramide.getAltura());
+		System.out.println("Raio: " + piramide.getRaio());
+		System.out.println("Tipo: " + piramide.getTipo());
+		System.out.println("--------------------------------------");
+		System.out.println("Área da base: " + piramide.areaBase());
+		System.out.println("Área do triângulo: " + piramide.areaTriangulo());
+		System.out.println("Área Total: " + piramide.areaTotal());
+		System.out.println("Volume: " + piramide.Volume());
+		System.out.println("--------------------------------------");
+		System.out.println("Quantidade de litros: " + piramide.Litros());
+		System.out.println("Quantidade de latas: " + piramide.Latas());
+		System.out.println("Valor Total: " + piramide.Gasto());
+		
 		valor.close();
 	}
 
--- Piramide.java
+++ Piramide.java
@@ -19,18 +19,48 @@ public class Piramide {
 	}
 	
 	public double areaBase() {
-		double area = Math.pow(raio, 2);
+		double area = Math.pow((raio * 2), 2);
 		return area;
 	}
 	
 	public double areaTriangulo() {
-		double area = (areaBase() * getAltura())/2;
+		double area = (Pitagoras() * getAltura())/2;
 		return area;
 	}
 	
 	public double areaTotal() {
 		double area = (areaTriangulo() * 4) + areaBase();
 		return area;
+	}
+	
+	public double Volume() {
+		double volume = (areaBase() * getAltura())/3;
+		return volume;
+	}
+	
+	public double Litros() {
+		double litros = areaTotal()/4.76;
+		return litros;
+	}
+	
+	public int Latas() {
+		double latas = Litros()/18;
+		int conversao = (int) Math.round(latas);
+		return conversao;
+	}
+	
+	public double Gasto() {
+		double gasto = 0;
+		if (getTipo() == 1) {
+			gasto = 127.90 * Latas();
+		}
+		else if (getTipo() == 2) {
+			gasto = 258.98 * Latas();
+		}
+		else {
+			gasto = 344.34 * Latas();
+		}
+		return gasto;
 	}
 
 	public float getAltura() {
